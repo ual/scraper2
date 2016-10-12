af59886b0f9d401680d52b97eeba3945dbe0120f
Max Gardner <gardner.max@gmail.com>
2016-10-11 17:20:48 -0700

    charity engine testing

--- test_scraper.py
+++ test_scraper.py
@@ -1,23 +1,24 @@
+# This is a test script for the rental listing scraper
+from datetime import datetime as dt
+from datetime import timedelta
+import pandas as pd
+import sys
+sys.path.insert(0, 'scraper2/')
+import scraper2
+
 __author__ = "Sam Maurer, UrbanSim Inc"
 __date__ = "May 6, 2016"
 
-# This is a test script for the rental listing scraper
-
-from datetime import datetime as dt
-from datetime import timedelta
 
 # add subfolder to system path
-import sys
-sys.path.insert(0, 'scraper2/')
 
-import scraper2
-
-
-domains = ['http://atlanta.craigslist.org/search/apa']
+domains = pd.read_csv('domains.txt', header=None, names=['domain'])
+domains = list(domains.domain)
+lookback = 2  # hours
 
 s = scraper2.RentalListingScraper(
-		domains = domains,
-		earliest_ts = dt.now() + timedelta(hours=2),
-		latest_ts = dt.now() + timedelta(hours=3))
+    domains=domains,
+    earliest_ts=dt.now() - timedelta(hours=lookback),
+    latest_ts=dt.now() + timedelta(hours=0))
 
 s.run()
